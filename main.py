b45a294468bb086bb43ebe21b64709f0ec4c01fe
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-02-11 22:34:03 +0000

    INIT

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as 
+
+%matplotlib inline
+sns.set_style("whitegrid")
+plt.style.use("fivethirtyeight")
+
+# example text for model training (SMS messages)
+simple_train = ['call you tonight', 'Call me a cab', 'Please call me... PLEASE!']
