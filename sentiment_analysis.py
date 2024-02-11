b1c40146d44c0b9880b5a89f10ee04844298c7a0
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-02-11 23:42:37 +0000

    Sentiment Analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -11,6 +11,19 @@ def preprocess_text(text):
     # Tokenize the text
     tokens = word_tokenize(text.lower())
 
+    # Remove stop words
+    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+    # Lemmatize the to-kens
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+    # Join the tokens back into a string
+
+    processed_text = ' '.join(lemmatized_tokens)
+
+    return processed_text
+
 
 # download nltk corpus (first time only)
 nltk.download('all')
@@ -20,3 +33,23 @@ df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datas
 
 print('AMAZON REVIEW DATASET:')
 print(df.head())
+
+# Preprocess the review text
+df['reviewText'] = df['reviewText'].apply(preprocess_text)
+
+print(df)
+
+# initialize NLTK sentiment analyzer
+analyzer = SentimentIntensityAnalyzer()
+
+# create get_sentiment function
+
+def get_sentiment(text):
+    scores = analyzer.polarity_scores(text)
+    sentiment = 1 if scores['pos'] > 0 else 0
+    return sentiment
+
+# apply get_sentiment function
+df['sentiment'] = df['reviewText'].apply(get_sentiment)
+
+print(df)
