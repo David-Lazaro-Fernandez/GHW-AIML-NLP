af8dc0663616deecc16d5259555b22aa1b50a833
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-02-11 22:56:34 +0000

    Vectorization of our vocabulary

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as 
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
 
-%matplotlib inline
+
 sns.set_style("whitegrid")
 plt.style.use("fivethirtyeight")
 
 # example text for model training (SMS messages)
 simple_train = ['call you tonight', 'Call me a cab', 'Please call me... PLEASE!']
+
+vect = CountVectorizer()
+vect.fit(simple_train)
+vect.get_feature_names_out()
+
+
+
