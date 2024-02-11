7751a30fce5aa66269b08e50d07ed38f1bb5a248
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-02-11 23:15:12 +0000

    TEST DATAFRAME

--- main.py
+++ main.py
@@ -23,4 +23,18 @@ print(simple_train_dtm.toarray())
 
 # examine the vocabulary and document-term matrix together
 df = pd.DataFrame(simple_train_dtm.toarray(), columns=vect.get_feature_names_out())
+print('TRAIN DATAFRAME:')
 print(df)
+
+
+# example text for model testing
+simple_test = ["please don't call me"]
+
+# transform testing data into a document-term matrix (using existing vocabulary)
+simple_test_dtm = vect.transform(simple_test)
+simple_test_dtm.toarray()
+
+test_df = pd.DataFrame(simple_test_dtm.toarray(), columns=vect.get_feature_names_out())
+
+print('TEST DATAFRAME:')
+print(test_df)
