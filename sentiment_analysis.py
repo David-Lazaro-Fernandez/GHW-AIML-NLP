083642a0f84c2675d13c7e464e822e5f7dd28074
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-02-11 23:18:58 +0000

    INIT Sent Analysis

---
+++ sentiment_analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
